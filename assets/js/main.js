794a06e981d506c0ee2ae79d42d45841cf09ea0a
sazedul-islam <132539686+sazedul-islam@users.noreply.github.com>
2023-09-23 17:23:27 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    startPosition:0,
+    autoplay:true,
+    autoplayTimeout:5000,
+    animationOut:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
